6bcb15888c6429fbbbcf88359f421a7737fa06fa
Mutant Admin <admin@mutant.city>
2020-08-23 20:12:11 -0500

    Update

--- functions/functions.sh
+++ functions/functions.sh
@@ -3,14 +3,25 @@ echo "Whats up!"
 
 # $1=aws region $2=aws_account_id
 function mutant-city-ecr-authenticate {
-  error_msg="Parameters required: region aws_account_id"
+  error_msg='Parameters required: $1=region $2=aws_account_id'
   [ -z "$1" ] && echo "$error_msg" && return
   [ -z "$2" ] && echo "$error_msg" && return
   aws ecr get-login-password --region "$1" | docker login --username "AWS" --password-stdin "$2.dkr.ecr.$1.amazonaws.com"
   true;
 }
+# $1=container id $2=account_id $3=region $4=name of ecr repo
+function mutant-city-ecr-push {
+  error_msg='Parameters required: $1=container id $2=account_id $3=region $4=name of ecr repo'
+  [ -z "$1" ] && echo "$error_msg" && return
+  [ -z "$2" ] && echo "$error_msg" && return
+  [ -z "$3" ] && echo "$error_msg" && return
+  [ -z "$4" ] && echo "$error_msg" && return
+  url="$2.dkr.ecr.$3.amazonaws.com/$4"
+  docker tag $1 $url
+  docker push $url
+}
 
-# $1=character length
+# $1=length, default 10
 function mutant-city-generate-random-hash {
   local size
   local out
@@ -26,6 +37,7 @@ function mutant-city-generate-ssh-key {
   ssh-keygen -t rsa -b 4096 -f "$filename" -q -N ""
   echo "Key Generated: $random_num and $random_num.pub"
 }
+
 # $1=git message
 function mutant-city-git-push-all {
   [ -z "$1" ] && message="Update" || message=$1
