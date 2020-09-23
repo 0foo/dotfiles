bcd83d8c9ce3eadd9f5ba3e68cf1c2efb6064ae3
mutant-city <6382763+mutant-city@users.noreply.github.com>
2020-09-23 14:58:18 -0500

    Update function-dependencies.sh

--- functions/function-dependencies.sh
+++ functions/function-dependencies.sh
@@ -1,3 +1,9 @@
 # aws cli - https://aws.amazon.com/cli/
 # hyperjump - add to /etc/profile.d - https://github.com/x0054/hyperjump
 # keychain - https://www.funtoo.org/Keychain
+
+
+
+# SSH 
+# On systems with only one user to share ssh keys with root, can create a group and create a directory in /etc/ssh, and then symlink that directory as the .ssh folder for 
+#   main user and root, be sure to reboot after adding to the group!
