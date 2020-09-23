fff103a6d8dace1b84dcea36235ef04d2b4b75bd
mutant-city <6382763+mutant-city@users.noreply.github.com>
2020-09-23 14:48:00 -0500

    Update functions.sh

--- functions/functions.sh
+++ functions/functions.sh
@@ -49,5 +49,11 @@ function mutant-city-git-push-all {
   git push
 }
 
-
+# add keys to keychain
+# requires keychain dependency
+# https://www.funtoo.org/Keychain
+function mutant-city-add-ssh-keys-to-keychain{
+  KEYS=$(find ~/.ssh -maxdepth 1 -type f -not -name '*.pub' -type f -not -name 'known_hosts' -not -name 'config')
+  eval `keychain --eval --agents ssh $KEYS`
+}
 
