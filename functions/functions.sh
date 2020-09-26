2bf97abdf880446887a4ed1c26e603db4e395d72
Mutant Admin <admin@mutant.city>
2020-09-26 08:31:47 -0500

    Update

--- functions/functions.sh
+++ functions/functions.sh
@@ -1,31 +1,7 @@
 #!/bin/bash
-echo "Whats up!"
-
-# $1=aws region $2=aws_account_id
-function mutant-city-ecr-authenticate {
-  error_msg='Parameters required: $1=region $2=aws_account_id'
-  [ -z "$1" ] && echo "$error_msg" && return
-  [ -z "$2" ] && echo "$error_msg" && return
-  aws ecr get-login-password --region "$1" | docker login --username "AWS" --password-stdin "$2.dkr.ecr.$1.amazonaws.com"
-  true;
-}
-# $1=image id $2=account_id $3=region $4=name of ecr repo
-function mutant-city-ecr-push {
-  error_msg='Parameters required: $1=image id $2=account_id $3=region $4=name of ecr repo'
-  [ -z "$1" ] && echo "$error_msg" && return
-  [ -z "$2" ] && echo "$error_msg" && return
-  [ -z "$3" ] && echo "$error_msg" && return
-  [ -z "$4" ] && echo "$error_msg" && return
-
-  aws ecr create-repository --repository-name "$4" --image-scanning-configuration scanOnPush=true --region "$3"
-  wait $!
-  url="$2.dkr.ecr.$3.amazonaws.com/$4"
-  docker tag $1 $url
-  docker push $url
-}
 
 # $1=length, default 10
-function mutant-city-generate-random-hash {
+function mutant-encrypt-random-hash {
   local size
   local out
   [ -z "$1" ] && size=10 || size=$1
@@ -33,7 +9,7 @@ function mutant-city-generate-random-hash {
   echo "$out"
 }
 
-function mutant-city-generate-ssh-key {
+function mutant-ssh-generate-key {
   local random_num
   random_num="$(mutant-city-generate-random-hash 10)"
   filename="/home/$USER/.ssh/$random_num"
@@ -42,18 +18,12 @@ function mutant-city-generate-ssh-key {
 }
 
 # $1=git message
-function mutant-city-git-push-all {
+function mutant-git-push-all {
   [ -z "$1" ] && message="Update" || message=$1
   git add .
   git commit -m $message
   git push
 }
 
-# add keys to keychain
-# requires keychain dependency
-# https://www.funtoo.org/Keychain
-function mutant-city-add-ssh-keys-to-keychain{
-  KEYS=$(find $HOME/.ssh/* -maxdepth 1 -type f -not -name '*.pub' -type f -not -name 'known_hosts' -not -name 'config')
-  eval `keychain --eval --agents ssh $KEYS`
-}
+
 
---
+++ main.sh
@@ -0,0 +1,3 @@
+source "$HOME/dotfiles/functions/functions.sh"
+source "$HOME/dotfiles/functions/functions-aws.sh"
+source "$HOME/dotfiles/functions/function-dependencies.sh"
