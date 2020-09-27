5ca403f42c9e9fa363717c85a5f05a6c90e6e859
Mutant Admin <admin@mutant.city>
2020-09-27 06:35:32 -0500

    Update

--- functions/functions-only-posix.sh
+++ functions/functions-only-posix.sh
@@ -11,7 +11,7 @@ function mutant-encrypt-random-hash {
 
 function mutant-ssh-generate-key {
   local random_num
-  random_num="$(mutant-city-generate-random-hash 10)"
+  random_num="$(mutant-encrypt-random-hash 10)"
   filename="/home/$USER/.ssh/$random_num"
   ssh-keygen -t rsa -b 4096 -f "$filename" -q -N ""
   echo "Key Generated: $random_num and $random_num.pub"
--- main.sh
+++ main.sh
@@ -4,4 +4,5 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source "$ROOT_DIR/functions/functions-only-posix.sh"
 source "$ROOT_DIR/functions/aws/main.sh"
-source "$ROOT_DIR/functions/function-dependencies.sh"
+source "$ROOT_DIR/functions/functions-dependencies.sh"
+source "$ROOT_DIR/functions/functions-docker.sh"
