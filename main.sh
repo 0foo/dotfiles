c900b798a47a8e64d488f2ebd5203588cc5dcfa0
mutant-city <6382763+mutant-city@users.noreply.github.com>
2020-10-02 11:44:56 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@
 # https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-if [ $MUTANT_UPDATE_DOTFILES -ne 0 ]; then
+if [ -n "$MUTANT_UPDATE_DOTFILES" ]; then
   echo "Updating dotfiles"
   cd  $ROOT_DIR
   git fetch
