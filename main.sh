14a2d5f16388013700be65a72c28b555fb5ddbb2
mutant-city <6382763+mutant-city@users.noreply.github.com>
2020-10-02 10:26:41 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,8 +3,13 @@
 # this gets the directory of currently executing script
 # https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
 echo "Updating dotfiles"
-cd  $ROOT_DIR && git pull && cd -
+cd  $ROOT_DIR 
+git fetch
+git reset --hard origin/master 
+cd -
 
 source "$ROOT_DIR/functions/functions-only-posix.sh"
 source "$ROOT_DIR/functions/aws/main.sh"
