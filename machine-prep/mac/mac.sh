dc56664978e3fa9b18f299c5da499ef363579d64
Your Name <nkiermai@tulane.edu>
2021-06-15 19:08:41 -0600

    Update

--- machine-prep/mac/mac.sh
+++ machine-prep/mac/mac.sh
@@ -14,7 +14,13 @@ brew install the_silver_searcher
 
 # need to update the mac-os bash
 brew install bash
-sudo bash -c "echo '/usr/local/bin/bash' >> /etc/shells"
+sudo bash -c "echo '/usr/local/bin/bash' >> /etc/shells" # note: permissions require doing it this way
 chsh -s /usr/local/bin/bash
+
+# bash completion: https://github.com/kubernetes/kubernetes/issues/48575
+echo "source $(brew --prefix)/etc/bash_completion" >> ~/.bashrc
+
+
+# fini
 echo "CLOSE THE SHELL AND REOPEN FOR UPDATED BASH SHELL"
 
