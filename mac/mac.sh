48549a6f2ee53ce88e968dbfd70700a8e54fbaa1
Nick Kiermaier <nkiermai@nkiermai-mn1.linkedin.biz>
2022-04-10 16:36:46 -0700

    Update

--- mac/mac.sh
+++ mac/mac.sh
@@ -1,3 +1,6 @@
+# bash error reporting: https://codeinthehole.com/tips/bash-error-reporting/
+
+
 # this gets the directory of currently executing script
 # https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
--- mac/terminal-customize.sh
+++ mac/terminal-customize.sh
@@ -1,37 +1,150 @@
 #!/bin/bash
+function print_error {
+    read line file <<<$(caller)
+    echo "An error occurred in line $line of file $file:" >&2
+    sed "${line}q;d" "$file" >&2
+}
+trap print_error ERR
 
+#  Customize BASH PS1 prompt 
 
 # get git branch
 function parse_git_branch {
    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 
+
+# Wrap the variables in a function in order to keep the variables local to this script, otherwise by default bash var's are local
+# without this would have a ton of global variables in the shell after this finishes running
+function set_PS1 () {
+   local Color_Off="\[\033[0m\]"       # Text Reset
+
+   # Regular Colors
+   local Black="\[\033[0;30m\]"        # Black
+   local Red="\[\033[0;31m\]"          # Red
+   local Green="\[\033[0;32m\]"        # Green
+   local Yellow="\[\033[0;33m\]"       # Yellow
+   local Blue="\[\033[0;34m\]"         # Blue
+   local Purple="\[\033[0;35m\]"       # Purple
+   local Cyan="\[\033[0;36m\]"         # Cyan
+   local White="\[\033[0;37m\]"        # White
+
+   # Bold
+   local BBlack="\[\033[1;30m\]"       # Black
+   local BRed="\[\033[1;31m\]"         # Red
+   local BGreen="\[\033[1;32m\]"       # Green
+   local BYellow="\[\033[1;33m\]"      # Yellow
+   local BBlue="\[\033[1;34m\]"        # Blue
+   local BPurple="\[\033[1;35m\]"      # Purple
+   local BCyan="\[\033[1;36m\]"        # Cyan
+   local UBlack="\[\033[4;30m\]"       # Black
+   local URed="\[\033[4;31m\]"         # Red
+   local UGreen="\[\033[4;32m\]"       # Green
+   local UYellow="\[\033[4;33m\]"      # Yellow
+   local UBlue="\[\033[4;34m\]"        # Blue
+   local UPurple="\[\033[4;35m\]"      # Purple
+   local UCyan="\[\033[4;36m\]"        # Cyan
+   local UWhite="\[\033[4;37m\]"       # White
+
+   # Background
+   local On_Black="\[\033[40m\]"       # Black
+   local On_Red="\[\033[41m\]"         # Red
+   local On_Green="\[\033[42m\]"       # Green
+   local On_Yellow="\[\033[43m\]"      # Yellow
+   local On_Blue="\[\033[44m\]"        # Blue
+   local On_Purple="\[\033[45m\]"      # Purple
+   local On_Cyan="\[\033[46m\]"        # Cyan
+   local On_White="\[\033[47m\]"       # White
+
+   # High Intensty
+   local IBlack="\[\033[0;90m\]"       # Black
+   local IRed="\[\033[0;91m\]"         # Red
+   local IGreen="\[\033[0;92m\]"       # Green
+   local IYellow="\[\033[0;93m\]"      # Yellow
+   local IBlue="\[\033[0;94m\]"        # Blue
+   local IPurple="\[\033[0;95m\]"      # Purple
+   local ICyan="\[\033[0;96m\]"        # Cyan
+   local IWhite="\[\033[0;97m\]"       # White
+
+   # Bold High Intensty
+   local BIBlack="\[\033[1;90m\]"      # Black
+   local BIRed="\[\033[1;91m\]"        # Red
+   local BIGreen="\[\033[1;92m\]"      # Green
+   local BIYellow="\[\033[1;93m\]"     # Yellow
+   local BIBlue="\[\033[1;94m\]"       # Blue
+   local BIPurple="\[\033[1;95m\]"     # Purple
+   local BICyan="\[\033[1;96m\]"       # Cyan
+   local BIWhite="\[\033[1;97m\]"      # White
+
+   # High Intensty backgrounds
+   local On_IBlack="\[\033[0;100m\]"   # Black
+   local On_IRed="\[\033[0;101m\]"     # Red
+   local On_IGreen="\[\033[0;102m\]"   # Green
+   local On_IYellow="\[\033[0;103m\]"  # Yellow
+   local On_IBlue="\[\033[0;104m\]"    # Blue
+   local On_IPurple="\[\033[10;95m\]"  # Purple
+   local On_ICyan="\[\033[0;106m\]"    # Cyan
+   local On_IWhite="\[\033[0;107m\]"   # White
+
+   # Various variables you might want for your PS1 prompt instead
+   local Time12h="\T"
+   local Time12a="\@"
+   local PathShort="\w"
+   local PathFull="\W"
+   local NewLine="\n"
+   local Jobs="\j"
+   local User="\u"
+   local Host="\h"
+   local Git_branch="\$(parse_git_branch)"
+
+
+   # This PS1 snippet was adopted from code for MAC/BSD I saw from: http://allancraig.net/index.php?option=com_content&view=article&id=108:ps1-export-command-for-git&catid=45:general&Itemid=96
+   # I tweaked it to work on UBUNTU 11.04 & 11.10 plus made it mo' better
+
+   local ps1_terminal_string="$IYellow$User $IRed$PathShort $IGreen$Git_branch $IWhite: "
+   export PS1="$ps1_terminal_string"
+}
+# run the function
+set_PS1
+
+
+
 # configure coloring
-PS1='\[\033[01;34m\]\w\[\033[01;35m\] $(parse_git_branch)\[\033[01;39m\] : \[\033[01;32m\]'
 
+# # https://gist.github.com/vratiu/9780109
+# CUSTOMIZE_DIR_PS1="\w\[\033[01;37m\]"
+# CUSTOMIZE_USER_HOST_PS1="[\u@\h]\[\033[0;33m\]"
+# CUSTOMIZE_GIT_BRANCH=""
+# PS1="$CUSTOMIZE_USER_HOST_PS1 $CUSTOMIZE_DIR_PS1 $CUSTOMIZE_GIT_BRANCH : "
 
-# If this is an xterm set the title to user@host:dir
-case "$TERM" in
-xterm*|rxvt*)
-    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
-    ;;
-*)
-    ;;
-esac
+# #   [\033[01;39m\]   \[\033[01;32m\]
+# # If this is an xterm set the title to user@host:dir
+# case "$TERM" in
+# xterm*|rxvt*)
+#     PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+#     ;;
+# *)
+#     ;;
+# esac
 
 # some aliases
 alias ls="ls -G"
 # some ls aliases
+
 alias ll='ls -aGlF'
 alias la='ls -AG'
 alias l='ls -CFG'
 alias ld='ls -ld -- */'
 
 
+
+## HISTORY
 # don't put duplicate lines in the history. See bash(1) for more options
 # ... or force ignoredups and ignorespace
 HISTCONTROL=ignoredups:ignorespace
 shopt -s histappend
+
+
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 HISTSIZE=1000
 HISTFILESIZE=2000
@@ -40,3 +153,8 @@ HISTFILESIZE=2000
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
+
+# change ls colors
+# via this generator
+# https://geoff.greer.fm/lscolors/
+export LSCOLORS="BxFxCxDxBxEgEdAbAgAcAd"
